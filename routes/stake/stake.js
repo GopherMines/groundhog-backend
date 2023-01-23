6edb457d83d359ac0d995e9b8b5137d25c988141
GopherMines <113481489+GopherMines@users.noreply.github.com>
2023-01-23 15:11:10 +0100

    update

--- routes/stake/stake.js
+++ routes/stake/stake.js
@@ -11,7 +11,6 @@ const {
 const routes = require("express").Router();
 
 const isAuth = require("../../middlewares/isAuth/isauth");
-const isAdmin = require("../../middlewares/isAdmin/isadmin");
 
 routes.post("/", isAuth, stake);
 routes.get("/", isAuth, getStakesById);
@@ -19,6 +18,6 @@ routes.post("/unstake", isAuth, unStake);
 routes.post("/return", isAuth, returnNFT);
 routes.get("/return", isAuth, getReturnsById);
 routes.get("/returns", isAuth, getAllReturns);
-routes.get("/stakecsv", isAuth, isAdmin, getStakedCSV);
+routes.get("/stakecsv", isAuth, getStakedCSV);
 
 module.exports = routes;
